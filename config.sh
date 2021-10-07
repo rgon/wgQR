26e5cae0d1e6dad9d9e86b34020ca9efb9a0e796
Gonzalo Ruiz <rgon@rgon.es>
2021-10-07 14:42:55 +0200

    New config

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 
 CONFKEYDIR=clients
 SRVIP=`cat srvip.secret`
-SRVPUBKEY=`cat publickey.secret`
+SRVPUBKEY=`cat /etc/wireguard/publickey`
 
 # NOTE: a file called publickey has to be present in the same directory
 # NOTE: /etc/wireguard/${interface}.conf must exist
