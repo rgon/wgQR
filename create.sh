7a3307229285ae97c0da322883b53ec2c39d0270
Gonzalo Ruiz <rgon@rgon.es>
2021-12-10 03:47:36 +0100

    Ip counter error checking: empty file

--- create.sh
+++ create.sh
@@ -65,6 +65,11 @@ if [ -f "${CONFKEYDIR}/$interface/${CLIENTNAME}.conf" ]; then		# Client already 
 	echo "Peer already set up for interface. Using ip ${CLIENTIP}."
 else									# Client is new client
 	COUNTER=`cat ${CONFKEYDIR}/${interface}/currentIpCounter.int 2>/dev/null || echo 2`
+	if [ "$COUNTER" == "" ]; 
+		COUNTER=2
+	elif ["$COUNTER" -lt 2];
+		COUNTER=2
+	fi
 	BASEIP=$(cat /etc/wireguard/${interface}.conf | grep Address | cut -d "=" -f 2 | cut -d "/" -f 1 | tr -d " " | rev | cut -d "." -f2- | rev)
 	CLIENTIP="${BASEIP}.${COUNTER}"
 	echo $((COUNTER+1)) > ${CONFKEYDIR}/${interface}/currentIpCounter.int
